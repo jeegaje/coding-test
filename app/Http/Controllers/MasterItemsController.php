d2eb3f3f14c0620022c7c91c66c92c6b1fbd60e0
angga jiyan <anggajiyanfajar@gmail.com>
2026-03-31 14:39:00 +0700

    fixing filter price

--- app/Http/Controllers/MasterItemsController.php
+++ app/Http/Controllers/MasterItemsController.php
@@ -24,7 +24,14 @@ class MasterItemsController extends Controller
 
         if (!empty($kode)) $data_search = $data_search->where('kode', $kode);
         if (!empty($nama)) $data_search = $data_search->where('nama', 'LIKE', '%' . $nama . '%');
-        if (!empty($hargamin)) $data_search = $data_search->where('harga_beli', '>=', $hargamin)->where('harga_beli', '<=', $hargamax);
+
+        if (!empty($hargamin) && !empty($hargamax)) {
+           $data_search = $data_search->whereBetween('harga_beli', [(int)$hargamin, (int)$hargamax]);
+        } elseif (!empty($hargamin)) {
+            $data_search = $data_search->where('harga_beli', '>=', (int)$hargamin);
+        } elseif (!empty($hargamax)) {
+            $data_search = $data_search->where('harga_beli', '<=', (int)$hargamax);
+        }
 
         $data_search = $data_search->select('kode', 'nama', 'jenis', 'harga_beli', 'laba', 'supplier')->orderBy('id')->get();
 
